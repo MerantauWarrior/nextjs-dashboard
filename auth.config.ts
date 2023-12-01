17ba32640f3c2ec10db6a569fb95f50549603687
boriskleshch1 <boriskleshch@gmail.com>
2023-12-01 19:57:01 +0200

    final app 12

--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,24 @@
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
+  providers: [],
   pages: {
     signIn: '/login',
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      //console.log({auth})
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      //console.log({isLoggedIn})
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
+      console.log('Sono in ultimo return true')
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
-};
+} satisfies NextAuthConfig;
